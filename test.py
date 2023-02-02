0ebbbd22508781e77566a4c95ed9027b12c56fe8
Kirera Wainaina <richardwainainak@gmail.com>
2023-02-02 13:09:10 +0300

    Create test for iterate_through_rows func
    checks whether the iter_rows method is called on the worksheet

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ PSEUDOCODE
 '''
 
 import unittest
+from unittest.mock import MagicMock
 import openpyxl
 import main
 
@@ -20,6 +21,24 @@ class TestGetPayeWorksheet(unittest.TestCase):
         sheet = main.get_paye_worksheet(self.workbook)
         self.assertIsInstance(sheet, 
             openpyxl.worksheet.worksheet.Worksheet)
+
+class WorkSheet:
+
+    def iter_rows():
+        pass
+
+class TestIterateThroughRows(unittest.TestCase):
+
+    def setUp(self):
+        self.worksheet = WorkSheet()
+        self.worksheet.iter_rows = MagicMock()
+
+    def test_isCalled_withargs(self):
+        min_row, max_row, min_col, max_col =5, 468, 1, 28
+        main.iterate_through_rows(self.worksheet, min_row=min_row, 
+            max_row=max_row, min_col=min_col, max_col=max_col)
+        self.worksheet.iter_rows.assert_called_with(min_row=min_row, 
+            max_row=max_row, min_col=min_col, max_col=max_col)
 
 if __name__ == '__main__':
     unittest.main()
